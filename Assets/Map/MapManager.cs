46df4734b2c8e0576e4c22fc2c7f4a4d710aa953
fxd <mygoddess007700@gmail.com>
2022-12-13 05:15:00 +0800

    🎨 地图场景导入背包与商店

--- Assets/Map/MapManager.cs
+++ Assets/Map/MapManager.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class MapManager : MonoBehaviour
 {
-    
+    public Color ncu_blue;
+    public Color room_pink;
+    public Color wood1;
+    public Color wood2;
+    public Color toy1;
+    public Color toy2;
+    public Color cactus1;
+    public Color cactus2;
 }
