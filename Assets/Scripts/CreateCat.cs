ae19e46cdfd1be758bc22ed11be185c9cc09c8f5
1608874770@qq.com <mavnp321457986>
2022-12-13 11:45:23 +0800

    尝试随机生成猫猫

---
+++ Assets/Scripts/CreateCat.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateCat : MonoBehaviour
+{
+    /*
+     * Introduction：mavnp
+    */
+
+    // Start is called before the first frame update
+
+    private int i ;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (i=0; SaveManger.Instance.catNum > 0; SaveManger.Instance.catNum--,i++)
+        {
+            GameObject newClone = (GameObject)Instantiate(Resources.Load("Prefabs/Cat"), new Vector3(Random.Range(100, 1000), Random.Range(400, 1100), 0), Quaternion.Euler(new Vector3(0, 0, 0f)));
+            newClone.name = SaveManger.Instance.catColor[i,0].ToString();//赋予预制体在场景中的名字
+            newClone.GetComponent<SpriteRenderer>().color = new Color(SaveManger.Instance.catColor[i, 1] / 255f, SaveManger.Instance.catColor[i, 2] / 255f, SaveManger.Instance.catColor[i, 3] / 255f, 1);
+        }
+    }
+}
